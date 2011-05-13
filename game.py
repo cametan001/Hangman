8ea265d19b8f9ca59bd0c4506a5a8b6fee25c876
cametan001 <Masashi.Kameda+github@gmail.com>
2011-05-14 01:45:24 +0900

    PyQtでGUIHangmanを作る。

---
+++ game.py
@@ -0,0 +1,115 @@
+#! /usr/bin/env python
+# -*- coding: utf-8
+
+# Game クラスは、全体の調整と表示を受け持つ
+class Game:
+    def __init__(self):
+        self.theTarget = self.getTarget()
+        self.GuessType = Guess
+        self.outcome = True
+        self.guesses = []
+
+    # メインの関数
+    # 結果をチェックし、ゲームが終わっていれば処理を終了させる
+    def play(self):
+        self.displayStart()
+        while (self.outcome):
+            self.guesses.append(self.GuessType())
+            self.outcome = self.theTarget.eval(self.guesses[-1])
+            self.display(self.outcome)
+
+    # 正しい種類の正解を生成するには、
+    # このメソッドをオーバーライドしなければならない
+    def getTarget(self):
+        return Target()
+
+    # リプレイ
+    # 値を初期化し、最初の画面を再表示する
+    def reStart(self):
+        self.__init__()
+        self.play()
+
+    # プレイ方法の説明などの吐いたオープニング画面を表示する
+    def displayStart(self):
+        print """
+        抽象クラス: Game
+        プレイするゲーム用のサブクラスのインスタンスを作成しなければなりません!"""
+
+    # 結果に応じて適切な表示を行う
+    def display(self, outcome):
+        if not outcome:
+            self.outcome = False
+
+# プレイヤーから値(推測)を受け取り、
+# 評価に使われるデータを提供する
+class Guess:
+    def __init__(self):
+        self.theValue = raw_input("何か入力してください: ")
+
+    def value(self):
+        return self.theValue
+
+# プレイヤーに当てさせる正解のオブジェクトを生成する
+# プレイヤーが提示した推測が当たりかどうかをチェックする
+class Target:
+    def __init__(self):
+        self.goal = self.getTarget()
+
+    def getTarget(self):
+        return False
+
+    def getGoal(self):
+        return self.goal
+
+    def eval(self, aGuess):
+        return False
+
+# 単純な言葉当てゲーム - フレームワークのテスト用
+class NameGame(Game):
+    names = ['alan', 'fred', 'barney', 'heather', 'wilma', 'betty']
+    def __init__(self):
+        Game.__init__(self)
+        self.failMSG = "残念、はずれ。もう一度挑戦してね!"
+        self.successMSG = "おめでとう。%d 回挑戦して、正解の %s を当てました。"
+        self.theTarget = self.getTarget()
+        self.GuessType = NameGuess     # クラス参照を変更
+    def displayStart(self):
+        print ("\n\n****************************")
+
+    # TargetではなくNameTargetを取得するようにオーバーライドする
+    def getTarget(self):
+        return NameTarget()
+
+    def display(self, outcome):
+        Game.display(self, outcome)
+        if outcome:
+            print self.failMSG
+        else:
+            print self.successMSG % (len(self.guesses),
+                                     self.theTarget.getGoal())
+
+# 選択できる名前を表示し、デフォルトプロンプトを変更する
+class NameGuess(Guess):
+    def __init__(self):
+        print NameGame.names
+        self.theValue = raw_input("名前を入力してください: ")
+
+# プレイヤーに名前を当てさせ、
+# 正解になるまでの回数をカウントする
+class NameTarget(Target):
+    def getTarget(self):
+        import random
+        return NameGame.names[int(random.random() * \
+                                  (len(NameGame.names) - 0.001))]
+
+    def eval(self, aGuess):
+        if self.goal == aGuess.value():
+            return False
+        else:
+            return True
+
+# ゲームオブジェクトを作成し、プレイを開始する
+if __name__ == "__main__":
+    mygame = NameGame()
+    mygame.play()
+    # mygame.reStart()
---
+++ hangman.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import game, random, string, sys
+
+## Game
+class Hangman(game.Game):
+    wordfile = 'hangman.words'          # 同一フォルダ内に置かれているものとする
+    def __init__(self):
+        game.Game.__init__(self)
+        self.GuessType = hmGuess
+        self.outcome = 6                # outcomeで挑戦回数をカウントする
+
+    def displayStart(self):
+        self.display(6)
+
+    def getTarget(self):
+        return hmTarget()
+
+    def getResult(self):
+        # 現時点で提示された文字のリストを生成する
+        guessed = [g.value() for g in self.guesses if self.guesses]
+        # 正解と提示された文字と比較する
+        theWord = "".join([c in guessed and c or "_" for c in self.theTarget.getGoal()])
+        return theWord
+
+    def display(self, outcome):
+        theWord = self.getResult()
+        # 正しいかどうかをチェックする
+        if '_' in theWord and outcome == 0:
+            print "残念、はずれ。正解は「", \
+                  self.theTarget.getGoal(), "」でした。"
+        elif '_' not in theWord:
+            print "正解です。おめでとう!"
+            sys.exit()
+        else:
+            print "当てる言葉は %s\t あと %d 回挑戦出来ます。" \
+                  % (theWord, outcome)
+
+## Guess
+class hmGuess(game.Guess):
+    def __init__(self):
+        self.theValue = raw_input("次の文字: ")
+        # 入力が適切かどうかをチェックする
+        if len(self.theValue) > 1:      # 先頭の文字だけを使う
+            self.theValue = self.theValue[0]
+        if self.theValue not in string.letters:
+            self.theValue = raw_input("文字を入力してください!")
+
+## Target
+class hmTarget(game.Target):
+    def __init__(self):
+        self.outcome = 6
+        try:
+            wrdFile = open(Hangman.wordfile, "r")
+            wordList = wrdFile.readlines()
+            wrdFile.close()
+            index = int(random.random() * \
+                        (len(wordList) - 0.001))
+            self.goal = wordList[index][:-1] # \nを除去
+        except IOError:
+            print 'ファイル %s の読み取りに失敗しました。' \
+                  % Hangman.wordfile
+            sys.exit()
+
+    # evalは、あと何回間違ってもよいかの回数を返す
+    def eval(self, aGuess):
+        if aGuess.value() not in self.goal:
+            self.outcome -= 1
+        return self.outcome
+
+if __name__ == '__main__':
+    hangman = Hangman()
+    hangman.play()
